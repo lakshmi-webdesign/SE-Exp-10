ee68e446228fc777548453e50745e219468c2afe
lakshmi-webdesign <67644384+lakshmi-webdesign@users.noreply.github.com>
2021-05-17 13:28:23 +0530

    Update SEprog.c

--- SEprog.c
+++ SEprog.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 void main()
 {
-    int a,b,sum;
+    int a,b,sum ,diff;
     printf("Enter two numbers");
     scanf("%d %d",&a,&b);
     sum=a+b;
     printf("%d",sum);
+    
+    //added extra 2 lines
+    diff=a-b;
+    printf("%d",diff);
 }
